012372e6b669f81f208a8a3b7ad6c5f542537a23
Saryn Mooney <smooney15@my.whitworth.edu>
2020-07-08 09:38:27 -0600

    iteration 3 complete

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -16,4 +16,24 @@ class ShoppingCart
               :capacity => @capacity.delete("items").to_i}
   end
 
+  def total_number_of_products
+    @products.sum do |product|
+      product.quantity
+    end
+  end
+
+  def is_full?
+    if total_number_of_products > @capacity.to_i
+      true
+    else
+      false
+    end
+  end
+
+  def products_by_category(category)
+    @products.select do |product|
+      product.category == category
+    end 
+  end
+
 end
